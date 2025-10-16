5b9b770335779112922af5799a2796f4503f6cbd
Elleth68 <elleth@abv.bg>
2025-10-17 02:20:39 +0300

    Render cats conditionally

--- src/index.js
+++ src/index.js
@@ -62,9 +62,13 @@ function readFile(path) {
 async function homeView() {
 	const html = await readFile('./src/views/home/index.html');
 
-	const catsHtml = cats.map(cat => catTemplate(cat)).join('\n');
+	let catsHtml = '';
+	if (cats.length === 0) {
+		catsHtml = "No cats available"
+	} else {
+		catsHtml = cats.map(cat => catTemplate(cat)).join('\n');
+	}
 	const result = html.replace('{{cats}}', catsHtml);
-
 	return result;
 }
 
